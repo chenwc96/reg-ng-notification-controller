a5e0a1fe1cf31e4e5ee046a5fab850d303ed7f89
chenwc <weechung.chen@coder.com.my>
2025-08-20 18:44:35 +0800

    ...

--- dist/ng-notification-controller/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="@reg/ng-notification-controller" />
-export * from './public-api';
--- dist/ng-notification-controller/lib/http-service/request-header.enum.d.ts
+++
@@ -1,8 +0,0 @@
-export declare enum RequestHeader {
-    AUTHORIZATION = "AUTHORIZATION",
-    X_API_KEY = "X-API-KEY",
-    CONTENT_TYPE = "Content-Type",
-    ORIGIN = "Origin",
-    X_MOBILE_APP = "X-MOBILE-APP",
-    X_MOBILE_PLATFORM = "X-MOBILE-PLATFORM"
-}
